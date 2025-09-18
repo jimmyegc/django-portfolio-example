6a295867cf329ec47ea8c6cdfa3b637be2cdf070
Jimmy Ernesto García Contreras <872302+jimmyegc@users.noreply.github.com>
2025-09-17 19:55:08 -0600

    Update settings.py

--- django_portfolio/settings.py
+++ django_portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=8(4x4)q0tmkmqp%75zil6k#=hwh91es6%#y+y+0%v7$9c!fgv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
