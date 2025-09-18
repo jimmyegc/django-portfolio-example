e4cb99d6e03038d88f78d9051c6820cb85bebd6c
Jimmy García <jimmyegc@gmail.com>
2025-09-18 10:37:09 -0600

    Database django

--- django_portfolio/management/commands/initadmin.py
+++ django_portfolio/management/commands/initadmin.py
@@ -1,21 +1,15 @@
-
-
 from django.core.management.base import BaseCommand
-from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
+import os
 
 class Command(BaseCommand):
-    help = 'Crea un superusuario por defecto si no existe'
-
     def handle(self, *args, **kwargs):
-        User = get_user_model()
-        username = 'admin'      # Cambia esto si quieres otro usuario
-        email = 'admin@example.com'
-        password = 'admin123'   # Cambia la contraseña por algo seguro
+        username = os.getenv("DJANGO_SUPERUSER_USERNAME", "admin")
+        email = os.getenv("DJANGO_SUPERUSER_EMAIL", "admin@example.com")
+        password = os.getenv("DJANGO_SUPERUSER_PASSWORD", "admin123")
 
         if not User.objects.filter(username=username).exists():
             User.objects.create_superuser(username=username, email=email, password=password)
-            self.stdout.write(self.style.SUCCESS(f'Superusuario "{username}" creado correctamente.'))
+            self.stdout.write(self.style.SUCCESS(f"Superuser '{username}' created"))
         else:
-            self.stdout.write(self.style.WARNING(f'Superusuario "{username}" ya existe.'))
-
-
+            self.stdout.write(self.style.WARNING(f"Superuser '{username}' already exists"))
