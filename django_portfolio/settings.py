ab2bc70af3ae660b148a5a0747839ac3fab632df
Jimmy García <jimmyegc@gmail.com>
2025-09-18 11:26:06 -0600

    Database django

--- django_portfolio/settings.py
+++ django_portfolio/settings.py
@@ -139,7 +139,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "staticfiles", "media")
 
 # Hacer que Django copie también media dentro de collectstatic
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "media")]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "portfolio/static")]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
