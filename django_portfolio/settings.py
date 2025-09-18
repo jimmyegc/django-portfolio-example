1346250e1e57a5e1f9407d95d1bed90cf951d2bd
Jimmy García <jimmyegc@gmail.com>
2025-09-18 12:22:22 -0600

    Database django

--- django_portfolio/settings.py
+++ django_portfolio/settings.py
@@ -131,13 +131,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-
-# Agregamos la carpeta static de la app portfolio
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, "portfolio", "static")]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, "staticfiles", "media")
+MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
