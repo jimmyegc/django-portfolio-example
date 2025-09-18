9bd03cce9ffd5ede498afe6ed3309dab71749d6e
Jimmy García <jimmyegc@gmail.com>
2025-09-17 22:37:34 -0600

    Super user admin

--- app/management/commands/initadmin.py
+++ app/management/commands/initadmin.py
@@ -3,19 +3,20 @@ from django.contrib.auth.models import User
 import os
 
 class Command(BaseCommand):
-    help = 'Crea un superusuario automáticamente o actualiza la contraseña si ya existe'
+    help = 'Crea o actualiza automáticamente un superusuario'
 
     def handle(self, *args, **options):
         username = os.environ.get("DJANGO_SUPERUSER_USERNAME", "admin")
         email = os.environ.get("DJANGO_SUPERUSER_EMAIL", "admin@example.com")
         password = os.environ.get("DJANGO_SUPERUSER_PASSWORD", "admin123")
 
+        # Crear o actualizar superusuario
         user, created = User.objects.get_or_create(username=username)
         user.email = email
         user.is_staff = True
         user.is_superuser = True
         user.is_active = True
-        user.set_password(password)  # asegúrate de actualizar la contraseña
+        user.set_password(password)
         user.save()
 
         if created:
